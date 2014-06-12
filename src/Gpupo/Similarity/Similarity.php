f8312d15272d0f3c2c90da84fdbff747482feaaf
Gilmar Pupo <contact@gpupo.com>
2014-06-12 09:35:37 -0300

    This file

--- src/Gpupo/Similarity/Similarity.php
+++ src/Gpupo/Similarity/Similarity.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the similarity package.
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gpupo\Similarity;
 
 use Gpupo\Similarity\Input\InputString;
